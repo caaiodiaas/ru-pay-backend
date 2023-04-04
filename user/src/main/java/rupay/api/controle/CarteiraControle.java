c41ad56996d79b928403ccbc79d6fc7a05fe1304
GeorgFFV <georgeffv1@gmail.com>
2023-04-04 12:39:15 -0300

    CORS

--- user/src/main/java/rupay/api/controle/CarteiraControle.java
+++ user/src/main/java/rupay/api/controle/CarteiraControle.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import rupay.api.servico.CarteiraServico;
 
 
 @RestController
+@CrossOrigin("*")
 public class CarteiraControle {
     
     @Autowired
